5f33befb00524cdca2b07c1125510faa3f436b18
Caio Nascimento <101759288+icaiow@users.noreply.github.com>
2024-05-16 21:58:04 -0300

    atualizando o codigo

--- TrabalhoTesteDeSoftware/server.js
+++ TrabalhoTesteDeSoftware/server.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 // Rota para o cadastro de usuário
 app.post('/registro.html', (req, res) => {
   const { nome, email, senha } = req.body;
-  
+
   // Consulta para verificar se o e-mail já existe
   const SELECT_EMAIL_QUERY = `SELECT * FROM usuarios WHERE email = '${email}'`;
 
@@ -52,18 +52,19 @@ app.post('/registro.html', (req, res) => {
 
       // Se o e-mail não existe, insira o novo usuário no banco de dados
       const INSERT_USER_QUERY = `INSERT INTO usuarios (nome, email, senha) VALUES ('${nome}', '${email}', '${senha}')`;
-  
+
       connection.query(INSERT_USER_QUERY, (err, result) => {
           if (err) {
               console.error('Erro ao cadastrar usuário:', err);
               res.status(500).send('Erro interno no servidor');
               return;
           }
-          // Redireciona para a tela de login após o registro
-          res.redirect('/inicio.html');
+          // Envia uma mensagem de sucesso
+          res.status(200).send('Usuário cadastrado com sucesso');
       });
   });
 });
+
 
 
 // rota para o login de usuário
@@ -86,6 +87,40 @@ app.post('/login.html', (req, res) => {
     }
   });
 });
+
+
+/// Middleware para analisar corpos de solicitação
+app.use(express.json());
+
+// Rota para adicionar uma tarefa
+app.post('/adicionar-tarefa', (req, res) => {
+    const { title, description, time } = req.body;
+
+    // Validação dos dados da tarefa
+    if (!title || !time) {
+        return res.status(400).send('Por favor, preencha o título e o horário da tarefa.');
+        console.log(req.body);
+
+    }
+
+    // Cria um objeto de tarefa
+    const task = {
+        id: Date.now(),
+        title: title,
+        description: description || '',
+        time: time,
+        completed: false
+    };
+
+    // Adiciona a tarefa à lista de tarefas
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    // Retorna uma resposta de sucesso
+    res.status(200).send('Tarefa adicionada com sucesso.');
+});
+
+
 
 
 app.listen(port, () => {
